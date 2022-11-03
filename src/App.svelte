c992d429d0c11f97c6e17b142e074c4bfc1ea2c7
Hasaan Sultan <Hasaan.sultan@visionx.io>
2022-11-03 14:34:39 +0500

    svelte learning

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import FeedbackForm from "./components/FeedbackForm.svelte";
+  import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,",
+    },
+    {
+      id: 2,
+      rating: 8,
+      text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,",
+    },
+    {
+      id: 3,
+      rating: 5,
+      text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,",
+    },
+  ];
+
+  $: count = feedback.length;
+  $: average = (
+    feedback.reduce((a, { rating }) => a + rating, 0) / count
+  ).toFixed(2);
+  const deleteFeedback = (e) => {
+    console.log(e, "event");
+    feedback = feedback.filter((x) => x.id !== e.detail);
+  };
+</script>
+
+<main class="container">
+  <FeedbackForm />
+  <FeedbackStats {count} {average} />
+  <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+</main>
---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let count;
+  export let average;
+</script>
+
+<div class="feedback-state">
+  <h4>
+    {count} Review
+  </h4>
+  <h4>Ratings Average : {average}</h4>
+</div>
+
+<style>
+  .feedback-state {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
